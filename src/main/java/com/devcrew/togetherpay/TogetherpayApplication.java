13b0a1cc7711213be228758df2890d4cd36c75b3
nyeri9999 <neoyeo66@gmail.com>
2026-03-24 10:25:46 +0900

    [TogetherPay] init: 프로젝트 초기 설정 완료

---
+++ src/main/java/com/devcrew/togetherpay/TogetherpayApplication.java
@@ -0,0 +1,13 @@
+package com.devcrew.togetherpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcrew/togetherpay/TogetherpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcrew.togetherpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
