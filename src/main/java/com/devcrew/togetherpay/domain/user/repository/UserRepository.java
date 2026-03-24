09abfc3d614ec0e64a6b5ea97733eb362f845dca
nyeri9999 <neoyeo66@gmail.com>
2026-03-24 16:39:24 +0900

    [TogetherPay] fix: 유저 리포지토리명 오류 수정 및 패키지 함께 변경됨

--- src/main/java/com/devcrew/togetherpay/domain/user/repository/UserRepository.java
+++ src/main/java/com/devcrew/togetherpay/domain/user/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRespository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     // 소셜 로그인 시, 이메일로 기존 가입자인지 검증용으로 사용
     Optional<User> findByEmail(String email);
     // 소셜 로그인의 경우 이메일이 없는 경우가 있어, 소셜 제공자 타입 AND 식별자로 조회
