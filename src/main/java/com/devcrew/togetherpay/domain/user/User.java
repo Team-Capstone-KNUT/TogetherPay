e592a6a18d2bd9c672db2709bc99ebb7af520cdc
nyeri9999 <neoyeo66@gmail.com>
2026-03-24 10:36:11 +0900

    [TogetherPay] feat: 인증 서비스용 유저 리포지토리, 엔티티 구현 완료

---
+++ src/main/java/com/devcrew/togetherpay/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.devcrew.togetherpay.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    private String providerId;
+
+    @Builder
+    public User(String email, String nickname, UserRole role, Provider provider, String providerId) {
+        this.email = email;
+        this.nickname = nickname;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+
+    public void updateNickname(String newNickname) {
+        if (newNickname != null && !newNickname.isBlank()) {
+            this.nickname = newNickname;
+        }
+    }
+
+
+
+
+}
