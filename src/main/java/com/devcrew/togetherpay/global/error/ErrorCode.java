63db3c2ebc89b9701d768c47a1ef08d909484343
nyeri9999 <neoyeo66@gmail.com>
2026-03-27 10:51:44 +0900

    [TogetherPay] feat: 유저 에러 코드 정의 및 주석 변경

--- src/main/java/com/devcrew/togetherpay/global/error/ErrorCode.java
+++ src/main/java/com/devcrew/togetherpay/global/error/ErrorCode.java
@@ -7,11 +7,16 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    // 전반적 시스템 오류 정의
+    // 에러 코드 네이밍 부분은 도메인별로 접두사로 나누는 방법으로 진행.
+    // 공통(Common)
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C001", "서버 내부 오류입니다."),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C002", "잘못된 입력입니다."),
     INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "C003", "입력 타입이 유효하지 않습니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C004", "허용되지 않은 HTTP 메서드입니다.");
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C004", "허용되지 않은 HTTP 메서드입니다."),
+
+    // 유저(User)
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "존재하지 않는 회원입니다."),
+    USER_ALREADY_WITHDRAWN(HttpStatus.BAD_REQUEST, "U002", "이미 탈퇴 처리 된 회원입니다.");
 
     private final HttpStatus status;
     private final String code;
