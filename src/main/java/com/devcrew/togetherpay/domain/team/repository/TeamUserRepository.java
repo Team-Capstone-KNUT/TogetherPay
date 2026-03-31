addb3b6c64c976988c05d407bb70ddc98b16c054
nyeri9999 <neoyeo66@gmail.com>
2026-03-31 17:13:08 +0900

    [TogetherPay] feat: TeamUserRepository 권한 검사용 쿼리 메서드 구현

--- src/main/java/com/devcrew/togetherpay/domain/team/repository/TeamUserRepository.java
+++ src/main/java/com/devcrew/togetherpay/domain/team/repository/TeamUserRepository.java
@@ -5,6 +5,11 @@ import com.devcrew.togetherpay.domain.team.TeamUser;
 import com.devcrew.togetherpay.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamUserRepository extends JpaRepository <TeamUser, Long> {
+import java.util.Optional;
+
+public interface TeamUserRepository extends JpaRepository<TeamUser, Long> {
+    // 팀 가입 시의 중복 검사용 메서드
     boolean existsByTeamAndUser(Team team, User user);
+    // 예산/지출 등 도메인 로직 시 특정 팀원의 정보, 권한 조회 메서드
+    Optional<TeamUser> findByTeamAndUser(Team team, User user);
 }
