b6b83feb3658b370dd02fd98f4654def587bc726
nyeri9999 <neoyeo66@gmail.com>
2026-04-03 16:36:16 +0900

    [TogetherPay] feat: 팀 삭제 기능 구현 및 주석 추가

--- src/main/java/com/devcrew/togetherpay/domain/team/controller/TeamController.java
+++ src/main/java/com/devcrew/togetherpay/domain/team/controller/TeamController.java
@@ -10,34 +10,61 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/teams")
 @RequiredArgsConstructor
 public class TeamController {
     private final TeamService teamService;
+
     /**
      * 팀 생성 API
+     * @param userId
+     * @param request
+     * @return
      */
     @PostMapping
     public ResponseEntity<TeamResponse> createTeam(
             @AuthenticationPrincipal Long userId,
             @Valid @RequestBody CreateTeamRequest request) {
+        // 팀 비즈니스 로직에서 생성 메서드 호출하고 파라미터로 userId, 받은 request 안에 들어있는 name, password를 넘겨준다.
         Team team = teamService.createTeam(userId, request.name(), request.password());
+        // 응답값 response from 메서드 호출해서 자동 변환
         TeamResponse response = TeamResponse.from(team);
-
+        // response에 http 상태값도 같이 body에 담아서 전달
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    /**
+     * 팀 가입 API
+     * @param userId
+     * @param request
+     * @return
+     */
     @PostMapping("/join")
     public ResponseEntity<Void> joinTeam(
             @AuthenticationPrincipal Long userId,
             @Valid @RequestBody JoinTeamRequest request) {
+        // 비즈니스 로직 단 팀 참가 메서드 호출, 파라미터로 userId, request 내의 inviteCode(초대코드), password(패스워드) 반환
         teamService.joinTeam(userId, request.inviteCode(), request.password());
 
         return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 팀 삭제 API
+     * @param userId
+     * @param teamId
+     * @return
+     */
+    @DeleteMapping("/{teamId}")
+    public ResponseEntity<Void> deleteTeam(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable Long teamId) {
+        // 팀 삭제 비즈니스 로직 호출, 파라미터로 userId와 teamId를 넘겨준다.
+        teamService.deleteTeam(userId, teamId);
+        // 응답 성공 시 204 no content 반환(반환값 없음)
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/devcrew/togetherpay/domain/team/service/TeamService.java
+++ src/main/java/com/devcrew/togetherpay/domain/team/service/TeamService.java
@@ -23,37 +23,82 @@ public class TeamService {
     private final TeamUserRepository teamUserRepository;
 
     /**
-     * 팀 개설 및 방장(Leader) 지정
+     * 팀 생성 비즈니스 로직
+     * @param userId
+     * @param name
+     * @param password
+     * @return
      */
     @Transactional
     public Team createTeam(Long userId, String name, String password) {
+        // 유저 검증 메서드 호출
         User user = getUserOrThrow(userId);
-
+        // 팀 생성 팩토리 메서드 호출(name, password)
         Team team = Team.createTeam(name, password);
+        // 위에서 생성한 팀 객체와 유저 객체를 팩토리 메서드로 전달, role까지 묶어서 만들어줌
         TeamUser.createLeader(team, user);
-
         return teamRepository.save(team);
     }
 
+    /**
+     * 팀 가입 비즈니스 로직
+     * @param userId
+     * @param inviteCode
+     * @param password
+     */
     @Transactional
     public void joinTeam(Long userId, String inviteCode, String password) {
+        // 유저 검증 메서드 호출
         User user = getUserOrThrow(userId);
+        // 팀 초대 코드가 일치한지 검증
         Team team = teamRepository.findByInviteCode(inviteCode)
                 .orElseThrow(() -> new BusinessException(ErrorCode.TEAM_NOT_FOUND));
-
+        // 해당 팀 패스워드가 일치한지 검증 메서드 호출
         team.validatePassword(password);
-
         // 멤버 중복 가입 방어 로직
         if (teamUserRepository.existsByTeamAndUser(team, user)) {
             throw new BusinessException(ErrorCode.ALREADY_TEAM_MEMBER);
         }
-
+        // 해당 유저를 팀에 추가시키기 위해서 createMember 메서드 호출. 파라미터로 팀이랑 유저 객체 함께 넘겨준다.
         TeamUser teamUser = TeamUser.createMember(team, user);
         teamUserRepository.save(teamUser);
     }
 
+    /**
+     * 팀 삭제 비즈니스 로직(리더만)
+     * @param userId
+     * @param teamId
+     */
+    @Transactional
+    public void deleteTeam(Long userId, Long teamId) {
+        // 유저 검증 메서드 호출
+        User user = getUserOrThrow(userId);
+        // 팀 검증 메서드 호출
+        Team team = getTeamOrThrow(teamId);
+        // 해당 유저가 팀에 속해있는지 여부 검증하는 메서드 호츌
+        TeamUser teamUser = getTeamUserOrThrow(team, user);
+        // 해당 유저가 리더인지 검증하는 메서드 호출.(방장이 아닌 경우 예외 발생)
+        teamUser.validateLeader();
+        // 팀 삭제
+        teamRepository.delete(team);
+        log.info("팀 삭제 완료. teamId: {}, deleteBy(userId): {}", teamId, userId);
+    }
+
+    // 유저 검증 메서드(유저 존재여부)
     private User getUserOrThrow(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    // 팀 검증 메서드(팀 존재여부)
+    private Team getTeamOrThrow(Long teamId) {
+        return teamRepository.findById(teamId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.TEAM_NOT_FOUND));
+    }
+
+    // 팀 유저 검증 메서드(유저가 팀에 속해 있는지 여부 확인)
+    private TeamUser getTeamUserOrThrow(Team team, User user) {
+        return teamUserRepository.findByTeamAndUser(team, user)
+                .orElseThrow(() -> new BusinessException(ErrorCode.TEAM_MEMBER_NOT_FOUND));
     }
 }
