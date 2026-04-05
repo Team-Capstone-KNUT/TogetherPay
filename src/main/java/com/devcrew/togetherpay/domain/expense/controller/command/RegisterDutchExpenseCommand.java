dc656020e5e52f8d83ba1155bb0059dd4570965c
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-04-05 17:00:44 +0900

    [TogetherPay] feat: 지출 등록 날짜 필드 추가

--- src/main/java/com/devcrew/togetherpay/domain/expense/controller/command/RegisterDutchExpenseCommand.java
+++ src/main/java/com/devcrew/togetherpay/domain/expense/controller/command/RegisterDutchExpenseCommand.java
@@ -5,7 +5,10 @@ import com.devcrew.togetherpay.domain.expense.Currency;
 import com.devcrew.togetherpay.domain.expense.Expense;
 import com.devcrew.togetherpay.domain.expense.PaymentMethod;
 import com.devcrew.togetherpay.domain.expense.dto.ParticipantInfo;
+import com.devcrew.togetherpay.domain.team.Team;
+import com.devcrew.togetherpay.global.common.vo.Money;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
 
@@ -16,18 +19,22 @@ public record RegisterDutchExpenseCommand(
     String description,
     Currency currency,
     Category category,
+    LocalDate expenseDate,
     PaymentMethod method,
     BigDecimal totalAmount,
     List<ParticipantInfo> participantInfos
 ) {
 
-  public Expense toEntity(Team team, BigDecimal totalAmount) {
+  public Expense toEntity(Team team, BigDecimal totalAmount, BigDecimal exchangeRate) {
     return Expense.builder()
         .title(title)
         .description(description)
+        .currency(currency)
         .category(category)
+        .expenseDate(expenseDate)
         .paymentMethod(method)
-        .totalAmount(totalAmount)
+        .totalAmount(Money.of(totalAmount))
+        .exchangeRate(Money.of(exchangeRate))
         .team(team)
         .build();
   }
