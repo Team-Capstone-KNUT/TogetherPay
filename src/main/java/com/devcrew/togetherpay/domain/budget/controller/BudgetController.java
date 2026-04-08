609b6c2183a1cf9bc578a6e95e1aef1bfa38d04d
nyeri9999 <neoyeo66@gmail.com>
2026-04-08 14:07:17 +0900

    [TogetherPay] fix: requestMapping / 미입력 수정

--- src/main/java/com/devcrew/togetherpay/domain/budget/controller/BudgetController.java
+++ src/main/java/com/devcrew/togetherpay/domain/budget/controller/BudgetController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/teams/budgets")
+@RequestMapping("/api/v1/teams/budgets")
 @RequiredArgsConstructor
 public class BudgetController {
     private final BudgetService budgetService;
