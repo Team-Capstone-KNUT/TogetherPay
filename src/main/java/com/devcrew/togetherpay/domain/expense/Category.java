d6ec0eaa501c615016ec6f34cd4c35bb5ac2a6eb
nyeri9999 <neoyeo66@gmail.com>
2026-05-10 18:10:05 +0900

    [TogetherPay] fix: 지출 카테고리 수정 및 추가

--- src/main/java/com/devcrew/togetherpay/domain/expense/Category.java
+++ src/main/java/com/devcrew/togetherpay/domain/expense/Category.java
@@ -5,5 +5,9 @@ public enum Category {
   SNACK, // 간식
   TRANSPORT, // 교통
   ACCOMMODATION, // 숙박
+  SIGHTSEEING, // 관광
+  FLIGHT, // 항공
+  SHOPPING, // 쇼핑
+  ACTIVITY, // 액티비티
   MISCELLANEOUS // 기타
 }
