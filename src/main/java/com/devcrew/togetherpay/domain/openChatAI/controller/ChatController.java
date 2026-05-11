f720bb38e8a8c574ec120d6cfedf86bd3bc4fe58
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-05-11 15:24:32 +0900

    [TogetherPay] feat: @RequestBody 추가

--- src/main/java/com/devcrew/togetherpay/domain/openChatAI/controller/ChatController.java
+++ src/main/java/com/devcrew/togetherpay/domain/openChatAI/controller/ChatController.java
@@ -5,6 +5,7 @@ import com.devcrew.togetherpay.domain.openChatAI.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class ChatController {
   private final ChatService chatService;
 
   @RequestMapping("/chat")
-  public ResponseEntity<ChatResponse> chat(ChatRequest request) {
+  public ResponseEntity<ChatResponse> chat(@RequestBody ChatRequest request) {
 
     return ResponseEntity.ok(chatService.chat(request));
 
