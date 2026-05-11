a564f4f4d6d504a04f5f82ed8f48478f50798ff5
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-05-11 15:43:32 +0900

    [TogetherPay] fix: 코드 오류 수정

--- src/main/java/com/devcrew/togetherpay/domain/openChatAI/controller/ChatController.java
+++ src/main/java/com/devcrew/togetherpay/domain/openChatAI/controller/ChatController.java
@@ -5,17 +5,19 @@ import com.devcrew.togetherpay.domain.openChatAI.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequiredArgsConstructor
-@RestController("/api/v1/openChatApi")
+@RequestMapping("/api/v1/openChatAI")
 public class ChatController {
 
   private final ChatService chatService;
 
-  @RequestMapping("/chat")
+  @PostMapping("/chat")
   public ResponseEntity<ChatResponse> chat(@RequestBody ChatRequest request) {
 
     return ResponseEntity.ok(chatService.chat(request));
