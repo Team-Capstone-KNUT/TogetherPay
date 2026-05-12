0bf90d710e29947e289039fbe31de644a8413634
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-05-13 02:35:58 +0900

    [TogetherPay] feat: ScheduleRepository 구현

---
+++ src/main/java/com/devcrew/togetherpay/domain/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,15 @@
+package com.devcrew.togetherpay.domain.schedule.repository;
+
+import com.devcrew.togetherpay.domain.schedule.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    Optional<Schedule> findByIdAndTripId(Long scheduleId, Long tripId);
+
+
+}
