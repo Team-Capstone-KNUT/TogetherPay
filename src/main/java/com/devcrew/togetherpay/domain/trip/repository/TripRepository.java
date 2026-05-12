0c07fe84fd7a395df78aebde7e68a78eb4cff5a1
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-05-13 02:44:37 +0900

    [TogetherPay] feat: 팀에 tripId가 속하는 trip 조회 기능 추가

--- src/main/java/com/devcrew/togetherpay/domain/trip/repository/TripRepository.java
+++ src/main/java/com/devcrew/togetherpay/domain/trip/repository/TripRepository.java
@@ -4,10 +4,13 @@ import com.devcrew.togetherpay.domain.trip.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
     List<Trip> findAllByTeamId(Long teamId);
 
     // 팀 아이디로 여행 목록 조회 및 시작일 순 정렬
     List<Trip> findByTeamIdOrderByStartDateAsc(Long teamID);
+
+    Optional<Trip> findByIdAndTeamId(Long tripId, Long teamId);
 }
