1337cb9713e1b98cd9f2f801a02747bbbec44803
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-05-12 19:23:41 +0900

    [TogetherPay] feat: 사용자가 질문을 넣지 않고 요청할 때, 에러 메시지 반환 구현.

--- src/main/java/com/devcrew/togetherpay/domain/openChatAI/dto/request/ChatRequest.java
+++ src/main/java/com/devcrew/togetherpay/domain/openChatAI/dto/request/ChatRequest.java
@@ -1,5 +1,8 @@
 package com.devcrew.togetherpay.domain.openChatAI.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ChatRequest(
+    @NotBlank(message = "질문을 입력해주세요.")
     String question
 ) {}
