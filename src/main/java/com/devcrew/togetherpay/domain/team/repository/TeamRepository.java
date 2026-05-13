40ed776048630a28e1862aff49e6297d944075dd
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-05-13 10:34:46 +0900

    [TogetherPay] feat: 팀에 여행id가 속하는지 검증 메서드 추가 구현

--- src/main/java/com/devcrew/togetherpay/domain/team/repository/TeamRepository.java
+++ src/main/java/com/devcrew/togetherpay/domain/team/repository/TeamRepository.java
@@ -20,4 +20,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 
     // 팀에 유저가 속하는지 검증 메서드
     boolean existsByIdAndTeamUsers_userId(Long teamId, Long userId);
+
+    // 팀에 여행id가 속하는지 검증 메서드
+    boolean existsByIdAndTrips_Id(Long teamId, Long tripId);
 }
