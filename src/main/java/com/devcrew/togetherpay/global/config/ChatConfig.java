894fbbec1124c12361ffb6ef17e0f9573ce77c83
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-05-15 14:17:16 +0900

    [TogetherPay] feat: 기본 프롬프트 수정

--- src/main/java/com/devcrew/togetherpay/global/config/ChatConfig.java
+++ src/main/java/com/devcrew/togetherpay/global/config/ChatConfig.java
@@ -12,7 +12,8 @@ public class ChatConfig {
   public ChatClient chatClient(ChatClient.Builder builder) {
     return builder
         .defaultSystem("당신은 여행 정보를 제공하는 AI입니다. 사용자가 여행에 대해서 질문하면 답하세요. "
-            + "단, 여행에 상관없는 질문이면 '답변 불가'라고 답하세요.")
+            + "단, 여행에 상관없는 질문이면 '답변 불가'라고 답하세요."
+            + "(마크다운 형식으로 보내지마세요.)")
         .defaultOptions(ChatOptions.builder()
             .temperature(0.7) // 높을수록 창의적임. 같은 답을 최소화함.
             .maxTokens(1000) // 최대 길이 제한.
